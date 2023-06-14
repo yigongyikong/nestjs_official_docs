1bd1dafea9ef98ec69c89b7e75a30ccc3c1a2fb9
yigongyikong <yigongyikong@gmail.com>
2023-06-14 12:15:52 +0000

    [20230614_01][02_02_OVERVIEW-Controllers([Sub-Domain Routing][Scopes][Asynchronicity][Request payloads])_v0.0.0]

--- official-docs-example/src/cats/cats.controller.ts
+++ official-docs-example/src/cats/cats.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
 // import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Post()
-    create(): string {
-        return 'This action adds a new cat';
+    async create(@Body() createCatDto: CreateCatDto ) {
+        return `This action adds a new cat, 
+            ${createCatDto.name}, ${createCatDto.age}, ${createCatDto.breed}`;
     }
 
     @Get()
